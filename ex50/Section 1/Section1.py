e3930e53366584792d1ca1fb4eea64693a2a419f
Ethan Case <ethan.case@student.rjuhsd.us>
2018-10-24 20:28:46 +0000

    Unit-1

--- ex50/Section 1/Section1.py
+++ ex50/Section 1/Section1.py
@@ -32,4 +32,26 @@ n = int(input())
 print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
 print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
 
-# Two timestamps
+#Two timestamps
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+
+s1total = ((h1 * 60 * 60) + (m1 * 60) + s1)
+s2total = ((h2 * 60 * 60) + (m2 * 60) + s2)
+
+if s1total > s2total:
+    sTotal = s1total - s2total
+else:
+    sTotal = s2total - s1total
+    
+print(sTotal)
+
+#School Desks
+a = int(input())
+b = int(input())
+c = int(input())
+print(a // 2 + b // 2 + c // 2 + a % 2 + b % 2 + c % 2)
